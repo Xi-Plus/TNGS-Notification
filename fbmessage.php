632396d0635429f6794e4b1b5989255826a4e40d
xiplus <huangxuanyuxiplus@gmail.com>
2017-03-27 10:06:40 +0800

    fix bug: unread time update

--- fbmessage.php
+++ fbmessage.php
@@ -18,6 +18,7 @@ $users = $sth->fetchAll(PDO::FETCH_ASSOC);
 $sthmsg = $G["db"]->prepare("INSERT INTO `{$C['DBTBprefix']}msgqueue` (`tmid`, `message`, `time`, `hash`) VALUES (:tmid, :message, :time, :hash)");
 $sthok = $G["db"]->prepare("UPDATE `{$C['DBTBprefix']}news` SET `fbmessage` = '1' WHERE `hash` = :hash");
 $sthdel = $G["db"]->prepare("DELETE FROM `{$C['DBTBprefix']}news` WHERE `hash` = :hash");
+$sthread = $G["db"]->prepare("UPDATE `{$C['DBTBprefix']}user` SET `lastread` = :lastread WHERE `tmid` = :tmid");
 foreach ($newss as $news) {
 	if ($news["idx"] == 0) {
 		$msg = $news["text"];
@@ -33,6 +34,12 @@ foreach ($newss as $news) {
 		$res = $sthmsg->execute();
 		if ($res === false) {
 			WriteLog("[fbmsg][error][insque] tmid=".$user["tmid"]." msg=".$msg);
+			continue;
+		}
+		if (strtotime($user["lastread"]) > time()) {
+			$sthread->bindValue(":lastread", date("Y-m-d H:i:s"));
+			$sthread->bindValue(":tmid", $user["tmid"]);
+			$sthread->execute();
 		}
 	}
 	if ($news["idx"] == 0) {
@@ -56,12 +63,8 @@ $sth->execute();
 $row = $sth->fetchAll(PDO::FETCH_ASSOC);
 
 $sthdel = $G["db"]->prepare("DELETE FROM `{$C['DBTBprefix']}msgqueue` WHERE `hash` = :hash");
-$sthread = $G["db"]->prepare("UPDATE `{$C['DBTBprefix']}user` SET `lastread` = :lastread WHERE `tmid` = :tmid");
 foreach ($row as $msg) {
 	$res = SendMessage($msg["tmid"], $msg["message"]);
-	$sthread->bindValue(":lastread", date("Y-m-d H:i:s"));
-	$sthread->bindValue(":tmid", $msg["tmid"]);
-	$sthread->execute();
 	if ($res) {
 		$sthdel->bindValue(":hash", $msg["hash"]);
 		$res = $sthdel->execute();
